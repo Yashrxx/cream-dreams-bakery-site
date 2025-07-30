8f03b05f42dec773cc00877b957a5cd91a0ff657
Yashrxx <yashkumar.s2023@vitstudent.ac.in>
2025-07-30 21:09:52 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman or curl)
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
